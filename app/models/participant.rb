783110bfa2cbda46cf3703fe9f487806c8009fb0
Diego Avalon <diegoavalon@live.com>
2015-03-11 13:24:51 -0700

    adds customized quickview render views for all users

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,12 +1,7 @@
 class Participant < ActiveRecord::Base
-	# has_one :foot
-	# has_one :body
-	# has_one :hand
 
 	validates :fname, :presence => true
 	validates :lname, :presence => true
-
-
 
 	before_save :uppercase_fields
 
@@ -19,10 +14,44 @@ class Participant < ActiveRecord::Base
 	  self.email.downcase!
 	  self.sales_area.capitalize!
 	  self.discipline.capitalize!
-	  # self.participant_type.capitalize
-	  # self.gender.upcase
-	  # self.ethnicity.capitalize
-	  # self.tops_size.capitalize
-	  # self.alpha_bottoms.capitalize
+	end
+
+	def lh_glove_size
+
+		left_size = self.lh_middle_finger
+		
+		case left_size
+			when 7..7.4   then   left_size = "XXS"
+			when 7.5..7.8  then  left_size = "XS"
+			when 7.9..8.2 then   left_size = "S"
+			when 8.3..8.6 then   left_size = "M"
+			when 8.7..9.0 then   left_size = "L"
+			when 9.1..9.2 then   left_size = "XL"
+			when 9.5..9.8 then   left_size = "XXL"
+			when 9.9..10.3 then  left_size = "XXXL"
+		end
+
+		left_size
+
+	end
+
+
+	def rh_glove_size
+
+		right_size = self.rh_middle_finger
+		
+		case right_size
+			when 7..7.4   then   right_size = "XXS"
+			when 7.5..7.8  then  right_size = "XS"
+			when 7.9..8.2 then   right_size = "S"
+			when 8.3..8.6 then   right_size = "M"
+			when 8.7..9.0 then   right_size = "L"
+			when 9.1..9.2 then   right_size = "XL"
+			when 9.5..9.8 then   right_size = "XXL"
+			when 9.9..10.3 then  right_size = "XXXL"
+		end
+
+		right_size
+
 	end
 end
